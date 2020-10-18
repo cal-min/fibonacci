fe513f5065bb5bc3d9ef92079a9fe539f5955ddf
cal-min <calmin0987@gmail.com>
2020-10-18 10:38:16 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 'use strict';
+const memo = new Map();
+//n=0とn=1決まっているものなので最初から入れておく
+memo.set(0,0);
+memo.set(1,1);
+
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
-    }
-    return fib(n - 1) + fib(n - 2);
+    //メモにデータがあればそれを使う
+    if (memo.has(n)) {
+        return memo.get(n);
+    } 
+    //メモにデータがないので計算する
+    const value = fib(n-1) + fib(n-2);
+    memo.set(n,value);
+    return value;
 }
-const length = 40;
+const length = 1000;
 for (let i = 0;i <= length; i++) {
     console.log(fib(i));
 }
